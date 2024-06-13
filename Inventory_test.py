ac442cf9f767bb225dfe5323c3a9bde244d04aca
Charles <macancharles9@gmail.com>
2024-06-13 10:29:11 +0800

    ui

--- Inventory_test.py
+++ Inventory_test.py
@@ -9,22 +9,6 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__()
         uic.loadUi('Inventory_test.ui', self)
 
-    def setupUi(self):
-        central_widget = QWidget(self)
-        self.setCentralWidget(central_widget)
-
-        layout = QHBoxLayout(central_widget)
-
-        self.label = QLabel()
-        pixmap = QPixmap("static/logo.jpg")
-        self.label.setPixmap(pixmap)
-        self.label.setFixedSize(51, 41)
-
-        layout.addWidget(self.label)
-        layout.addStretch() 
-
-        self.setGeometry(100, 100, 800, 600)
-
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
